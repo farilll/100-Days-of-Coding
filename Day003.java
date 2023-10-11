916fbaf2db1225615f4e1b79ef462574cacb6460
farilll <146690181+farilll@users.noreply.github.com>
2023-10-11 18:56:15 +0800

    Day003

---
+++ Day003.java
@@ -0,0 +1,22 @@
+package Day003;
+
+public class Day003 {
+
+  public static void main(String[] args) {
+  String nama = "faril";
+  byte umur = 17;
+  float beratBadan = 56f;
+  double tinggiBadan = 160;
+  char jenisKelamin = 'L';
+  
+  
+  System.out.println("nama: "+nama);
+  System.out.println("Umur: "+umur+" Tahun");
+  System.out.println("Berat Badan: "+beratBadan);
+  System.out.println("Tinggi Badan: "+tinggiBadan+"cm");
+  System.out.println("jenis Kelamin: "+jenisKelamin);
+  
+
+  }
+
+}
