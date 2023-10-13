1a256ca37fe18055ce2a47fb8d48c9da83fbc545
farilll <146690181+farilll@users.noreply.github.com>
2023-10-13 11:55:50 +0800

    Day005.java

---
+++ Day005.java
@@ -0,0 +1,13 @@
+package Day005;
+
+public class main {
+
+  public static void main(String[] args) {
+   boolean a = true;
+   boolean b = false;
+   
+   boolean c = a && b;
+   
+   System.out.println(c);
+  }
+}
