a7bbe238e194dca7ba42bb6070dd6b27ee8fe87e
farilll <146690181+farilll@users.noreply.github.com>
2023-10-14 12:38:00 +0800

    Day006.java

---
+++ Day006.java
@@ -0,0 +1,15 @@
+package Day006;
+
+public class main {
+
+  public static void main(String[] args) {
+  boolean var1;
+  boolean var2;
+  
+  var1 = true;
+  var2 = false;
+  
+  System.out.println("var1 = " +var1);
+  System.out.println("var2 = " +var2);
+  }
+}
