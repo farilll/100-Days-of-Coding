0d8a57b0fd851d402fa3fe74c083f991a463f656
farilll <146690181+farilll@users.noreply.github.com>
2023-10-15 17:44:33 +0800

    Day007.java

---
+++ Day007.java
@@ -0,0 +1,14 @@
+package Day007;
+
+public class main {
+
+  public static void main(String[] args) {
+  boolean A = true;
+  boolean B = false;
+  boolean c = A || B;
+  
+  System.out.println("A = " + A);
+  System.out.println("B = " + B);
+  System.out.println("c = " + !c);
+  }
+}
