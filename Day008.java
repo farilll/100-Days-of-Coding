1356675a5ef32f73a27912c40c3abac7bb7217bb
farilll <146690181+farilll@users.noreply.github.com>
2023-10-16 13:52:48 +0800

    Day008.java

---
+++ Day008.java
@@ -0,0 +1,15 @@
+package Day008;
+
+public class main {
+
+  public static void main(String[] args) {
+   
+   int angka = 2;
+   int angkaa = 3;
+     
+   System.out.println("Hasil Bagi = " + (angka / angkaa));
+   System.out.println("Hasil Kurang = " + (angka - angkaa));
+   System.out.println("Hasil Tambah = " + (angka + angkaa));
+   System.out.println("Hasil kali = " + (angka * angkaa));
+  }
+}
