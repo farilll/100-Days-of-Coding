2aee83fec0445c2f92029a9d1452006f0654fa86
farilll <146690181+farilll@users.noreply.github.com>
2023-10-20 18:48:06 +0800

    Day012.java

---
+++ Day012.java
@@ -0,0 +1,18 @@
+package Day012;
+
+
+public class main {
+  public static void main(String[] args) {
+  char a = 'F';
+  char b = 'A';
+  char c = 'R';
+  char d = 'I';
+  char e = 'L';
+  
+  System.out.println(a);
+  System.out.println(b);
+  System.out.println(c);
+  System.out.println(d);
+  System.out.println(e);
+}
+}
