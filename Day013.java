b9d73da1f1d8cb0bbf614cd3d0e79e17ff1246f9
farilll <146690181+farilll@users.noreply.github.com>
2023-10-22 07:41:06 +0800

    Day013.java

---
+++ Day013.java
@@ -0,0 +1,12 @@
+package Day013;
+
+
+public class main {
+  public static void main(String[] args) {
+  int intData = 17;
+  Integer intDataa = 19;
+  
+  System.out.println("Ini tipe data primitif = " + intData);
+  System.out.println("Ini tipe data non primitif = " + intDataa);
+}
+}
