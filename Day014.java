c36c7f66c977c847cdd5a9ff85af4c9a25ee5fb7
farilll <146690181+farilll@users.noreply.github.com>
2023-10-22 19:13:48 +0800

    Non primitif

---
+++ Day014.java
@@ -0,0 +1,29 @@
+package Day012;
+
+import java.security.cert.LDAPCertStoreParameters;
+
+
+
+public class main {
+  public static void main(String[] args) {
+  String nama = "Charla";
+  Byte umur = 18;
+  Integer nim = 12344667;
+  Short angkatan = 2066;
+  Character kelas = 'B';
+  Long nik = 77777778778888L;
+  Float bb = 46.2f;
+  Double tb = 163.1;
+  Boolean iniBoolean = true;
+  
+  System.out.println("NAMA            : " + nama);
+  System.out.println("UMUR            : " + umur);
+  System.out.println("NIM             : " + nim);
+  System.out.println("ANGKATAN        : " + angkatan);
+  System.out.println("KELAS           : " + kelas);
+  System.out.println("NIK             : " + nik);
+  System.out.println("BERAT BADAN     : " + bb); 
+  System.out.println("TINGGI BADAN    : " + tb);
+  System.out.println("JOMBLO          : " + iniBoolean);
+}
+}
