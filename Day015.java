ebef391657fa6d8c87215ebbfa654c36f130723c
farilll <146690181+farilll@users.noreply.github.com>
2023-10-22 22:03:21 +0800

    Char & boolean non primitif

---
+++ Day015.java
@@ -0,0 +1,38 @@
+package Day015;
+
+import java.security.cert.LDAPCertStoreParameters;
+
+
+
+public class main {
+  public static void main(String[] args) {
+  
+  //char non primitif
+  Character a = 'C';
+  Character b = 'H';
+  Character c = 'A';
+  Character d = 'R';
+  Character e = 'L';
+  Character f = 'A';
+  
+  
+  //boolean non primitif
+  Boolean iniBoolean = true;
+  Boolean iniBooleann = false;
+  
+  
+  System.out.println("				" + a);
+  System.out.println("				" + b);
+  System.out.println("				" + c);
+  System.out.println("				" + d);
+  System.out.println("				" + e);
+  System.out.println("				" + f);
+  
+  System.out.println("=================");
+  
+  System.out.println("Boolean A = " + iniBoolean);
+  System.out.println("Boolean B = " + iniBooleann);
+  
+  
+}
+}
