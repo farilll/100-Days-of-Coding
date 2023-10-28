9aef496e24177e69ab1e4a9427c9a1dddce2485b
farilll <146690181+farilll@users.noreply.github.com>
2023-10-28 11:28:33 +0800

    Chat, boolean, & long non-primitif

---
+++ Day021.java
@@ -0,0 +1,18 @@
+package Day021;
+
+public class main {
+    
+    public static void main(String[] args) {
+        // Tipe data char menjadi non-primitif Character
+        Character nonPrimitiveChar = 'A';
+        System.out.println("Tipe data non-primitif Character	: " + nonPrimitiveChar);
+
+        // Tipe data boolean menjadi non-primitif Boolean
+        Boolean nonPrimitiveBoolean = true;
+        System.out.println("Tipe data non-primitif Boolean	  : " + nonPrimitiveBoolean);
+
+        // Tipe data long menjadi non-primitif Long
+        Long nonPrimitiveLong = 1234567890L;
+        System.out.println("Tipe data non-primitif Long			  : " + nonPrimitiveLong);
+    }
+}
