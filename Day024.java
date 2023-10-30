911c9e8c2ac4aa4127c1f4ef518fb94011ebf021
farilll <146690181+farilll@users.noreply.github.com>
2023-10-30 15:48:43 +0800

    MIN-MAX VALUE

---
+++ Day024.java
@@ -0,0 +1,12 @@
+package Day024;
+
+public class main{
+    public static void main(String[] args) {
+    
+       System.out.println("Int = " + Integer.MIN_VALUE + "-" + Integer. MAX_VALUE);
+       System.out.println("byte = " + Byte.MIN_VALUE + "-"+ Byte.MAX_VALUE);
+       System.out.println("float = " + Float.MIN_VALUE + "-" + Float.MAX_VALUE);
+       System.out.println("long = " + Long.MIN_VALUE + "-"+ Long.MAX_VALUE);
+       
+    }
+}
