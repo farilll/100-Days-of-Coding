b64016a003decb3a20aebd364e5baec9a4149a90
farilll <146690181+farilll@users.noreply.github.com>
2023-11-01 19:12:15 +0800

    Konversi tipe data primitif ke string

---
+++ Day025.java
@@ -0,0 +1,18 @@
+package Day025;
+public class main{
+    public static void main(String[] args) {
+    int angka = 49;
+    String a = Integer.toString(angka);
+    
+    double angkaa = 50.0;
+    String b = Double.toString(angkaa);
+    
+    System.out.println("String : " + a);
+    System.out.println("String : " + b);
+    
+    
+     
+    
+
+    }
+}
