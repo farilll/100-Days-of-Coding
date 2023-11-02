46783e08f842e8ed383cbe7f1c367d6cc3d3360c
farilll <146690181+farilll@users.noreply.github.com>
2023-11-02 18:34:22 +0800

    Konversi tipe data primitif ke String

---
+++ Day026.java
@@ -0,0 +1,31 @@
+package Day026;
+public class main{
+    public static void main(String[] args) {
+    long angka = 1256789123;
+    String a = Long.toString(angka);
+    
+    byte angkaa = 17;
+    String b = Byte.toString(angkaa);
+    
+    float angkaaa = 666f;
+    String c = Float.toString(angkaaa);
+    
+    short angka4 = 111;
+    String d = Short.toString(angka4);
+    
+    boolean iniBoolean = true;
+    String e = Boolean.toString(iniBoolean);
+    
+    char iniChar = 'X';
+    String f = Character.toString(iniChar);
+    
+    
+    System.out.println("String : " + a);
+    System.out.println("String : " + b);
+    System.out.println("String : " + c);
+    System.out.println("String : " + d);
+    System.out.println("String : " + e);    
+    System.out.println("String : " + f);      
+
+    }
+}
