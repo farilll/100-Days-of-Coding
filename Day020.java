e89065ac0e44c69f9aec4c4dae190e3666013bfe
farilll <146690181+farilll@users.noreply.github.com>
2023-11-04 11:08:54 +0800

    Non primitif

--- Day020.java
+++ Day020.java
@@ -1,11 +1,10 @@
 package Day020;
 public class main {
     public static void main(String[] args) {
-        // Menggunakan objek Integer daripada tipe data int
+        
         Integer number = new Integer(42);
         System.out.println("Nilai: " + number);
-        
-        // Menggunakan objek String daripada tipe data String
+    
         String text = new String("Halo, dunia!");
         System.out.println(text);
     }
