280dfe78c77e9e79cb69158d1e5a0b521cd156eb
farilll <146690181+farilll@users.noreply.github.com>
2023-11-06 07:24:58 +0800

    Type casting

---
+++ Day029.java
@@ -0,0 +1,19 @@
+package Day029;
+
+public class main{
+        public static void main(String[] args) {
+        
+        int intValue = 10;
+        double doubleValue = intValue; 
+        System.out.println("int value: " + intValue);
+        System.out.println("double value: " + doubleValue);
+        
+        System.out.print("\n");
+        
+        double doubleNum = 5.75;
+        int intNum = (int) doubleNum; 
+        System.out.println("double value: " + doubleNum);
+        System.out.println("int value: " + intNum);
+        
+    }
+}
