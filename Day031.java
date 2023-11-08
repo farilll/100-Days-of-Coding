0f4501eb72a3d388c099719c7385c8fc756c492a
farilll <146690181+farilll@users.noreply.github.com>
2023-11-08 19:00:44 +0800

    Perbandingan

---
+++ Day031.java
@@ -0,0 +1,16 @@
+package Day031;
+
+public class main{
+        public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        
+        System.out.println(a == b);     
+        System.out.println(a != b);      
+        System.out.println(a < b);      
+        System.out.println(a <= b);      
+        System.out.println(a > b);  
+        System.out.println(a >= b);
+                     
+    }
+}
