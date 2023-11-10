6e08a9167fb860600d8cdf0b8a2035fd00a604a3
farilll <146690181+farilll@users.noreply.github.com>
2023-11-10 19:35:30 +0800

    Penugasan

---
+++ Day033.java
@@ -0,0 +1,21 @@
+package Day033;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("=== PENUGASAN ===");
+        int a,b;        
+        a = 20;
+        b = 10;
+        
+        a += b;
+        System.out.println("Penjumlahan = " + a);
+        a -= b;
+        System.out.println("Pengurangan = " + a);
+        a *= b;
+        System.out.println("Perkalian = " + a);
+        a %= b;
+        System.out.println("Sisa bagi = " + a);
+
+        
+    }
+}
