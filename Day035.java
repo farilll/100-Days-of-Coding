4e89c99753bfc3d7fc6ab88003523437ea4e4720
farilll <146690181+farilll@users.noreply.github.com>
2023-11-12 19:52:18 +0800

    Percabangan

---
+++ Day035.java
@@ -0,0 +1,21 @@
+package Day035;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka = scanner.nextInt();
+
+        if (angka > 0) {
+            System.out.println("Angka positif");
+        } else if (angka < 0) {
+            System.out.println("Angka negatif");
+        } else {
+            System.out.println("Angka nol");
+        }
+    }
+}
+
