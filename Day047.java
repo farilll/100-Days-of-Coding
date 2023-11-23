f14fb7033fbfdc7aa9977fa715c972fb3cc34299
farilll <146690181+farilll@users.noreply.github.com>
2023-11-23 23:20:25 +0800

    Switch case

---
+++ Day047.java
@@ -0,0 +1,27 @@
+package cobasaja;
+
+public class main {
+
+    public static void main(String[] args) {
+        int pil;
+        System.out.println("===Menu Makanan===");
+        System.out.println("1.ayam gerek \n2.nasi goreng \n3.bakso \n4.pizza");
+        System.out.println("===Menu yang Anda pilih===");
+        switch (pil = 2){
+            case 1:
+                System.out.println("ayam geprek");
+                break;
+            case 2:
+                System.out.println("nasi goreng");
+                break;
+            case 3:
+                System.out.println("bakso");
+                break;
+            case 4:
+                System.out.println("pizza");
+                break;
+        }
+    }
+    
+
+}
