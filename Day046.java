57a44a72bd3913b91ba85994dfbcf362dcb21a33
farilll <146690181+farilll@users.noreply.github.com>
2023-11-23 23:18:30 +0800

    Array

---
+++ Day046.java
@@ -0,0 +1,16 @@
+package cobasaja;
+
+public class main {
+
+    public static void main(String[] args) {
+        String[] buah = new String[4];
+        
+        buah[0] = "jeruk";
+        buah[1] = "mangga";
+        buah[2] = "apel";
+        buah[3] = "durian";
+
+        System.out.println(buah[3]);
+
+    }
+}
