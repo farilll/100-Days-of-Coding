e1332c71556daec7865c4e597a394e1edb8d1c4e
farilll <146690181+farilll@users.noreply.github.com>
2023-11-26 17:31:40 +0800

    Each

---
+++ Day048.java
@@ -0,0 +1,14 @@
+package each;
+
+public class main {
+
+    public static void main(String[] args) {
+        char[] huruf = {'a', 'b', 'c'};
+        
+        for(char a : huruf){
+          System.out.println(a);
+        }
+    }
+    
+
+}
