0fa74dd32a371822c165b33d7ed759cf9a221141
farilll <146690181+farilll@users.noreply.github.com>
2023-11-27 21:04:41 +0800

    Array non primitif

---
+++ Day050.java
@@ -0,0 +1,18 @@
+package arraynonprimitif;
+
+public class main {
+
+    public static void main(String[] args) {
+        String[] mobil = new String [4];
+        
+        mobil[0] = "BMW";
+        mobil[1] = "LAMBORGINI";
+        mobil[2] = "FERRARI";
+        mobil[3] = "BUGATI";
+        
+        for(String x : mobil){
+            System.out.println(x);
+        }
+    }
+    
+}
