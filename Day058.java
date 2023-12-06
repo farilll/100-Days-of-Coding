ab032fcb72cb457c66a8a9ae661af0ec057bf749
farilll <146690181+farilll@users.noreply.github.com>
2023-12-06 21:19:57 +0800

    Prosedur

---
+++ Day058.java
@@ -0,0 +1,11 @@
+package prosedur;
+
+public class Prosedur {
+    public static void mahasusah() {
+        System.out.println("Halo");
+    }
+    public static void main(String[] args) {
+        mahasusah();
+    }
+    
+}
