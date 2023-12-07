a2b4d499ef0225c827921a5407832dd341b9ba8b
farilll <146690181+farilll@users.noreply.github.com>
2023-12-07 20:50:01 +0800

    Arrays

---
+++ Day060.java
@@ -0,0 +1,14 @@
+package arrayten;
+
+import java.util.Arrays;
+
+public class ArrayTen {
+
+    public static void main(String[] args) {
+        String[] nama = {"Bruno", "Meguire", "Garnacho"};
+        nama[1] = "Casemiro";
+
+        System.out.println(Arrays.toString(nama));
+    }
+
+}
