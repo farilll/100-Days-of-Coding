3728d97f807e904dcffd6d9505288b5ee859df34
farilll <146690181+farilll@users.noreply.github.com>
2023-12-13 19:03:59 +0800

    Looping

---
+++ Day066.java
@@ -0,0 +1,11 @@
+package looping;
+
+public class main {
+   
+    public static void main(String[] args) {
+        for(int i = 0; i<10; i+=3){
+          System.out.println(i);
+        }
+    }
+    
+}
