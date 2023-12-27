47e6f7e62da6fcc2e1a67db227ee47a5ff39c516
farilll <146690181+farilll@users.noreply.github.com>
2023-12-27 19:45:56 +0800

    Menampilkan angka genap menggunakan do while

---
+++ Day080.java
@@ -0,0 +1,14 @@
+package whilee;
+
+public class main {
+
+    public static void main(String[] args) {
+        int angka = 0;
+        
+        do{
+          System.out.println(angka);
+          angka += 2;
+        }while(angka <= 100);
+    }
+
+}
