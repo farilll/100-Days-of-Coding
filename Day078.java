67b0d057cdc26d7e97c5e404fce11565a4e273c0
farilll <146690181+farilll@users.noreply.github.com>
2023-12-27 13:53:28 +0800

    While

---
+++ Day078.java
@@ -0,0 +1,14 @@
+package while3;
+
+public class main {
+
+    public static void main(String[] args) {
+        int angka = 1;
+        while (angka < 5) {
+            System.out.println("angka ke - " + angka);
+            angka++;
+
+        }
+    }
+
+}
