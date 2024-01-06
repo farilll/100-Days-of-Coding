1bf4f10665d4a8fecbc356fc79133f3b98b1c752
farilll <146690181+farilll@users.noreply.github.com>
2024-01-06 15:14:29 +0700

    angka acak

---
+++ Day090.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Day90 {
+    public static void main(String[] args) {
+        Random x = new Random();
+        int acak;
+        acak = x.nextInt(10);
+        System.out.println(acak);
+        acak = x.nextInt(10);
+        System.out.println(acak);
+        acak = x.nextInt(10);
+        System.out.println(acak);
+        acak = x.nextInt(10);
+        System.out.println(acak);
+    }
+}
