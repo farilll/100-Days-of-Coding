d18fb11204f7c7edceb14869dc11c317d26f1001
farilll <146690181+farilll@users.noreply.github.com>
2024-01-09 17:53:06 +0700

    Array

--- Day091.java
+++ Day091.java
@@ -5,8 +5,6 @@ public class Day091 {
         String[] arrKota = { "Aceh", "Medan", "Padang", "Palembang",
                 "Riau" };
 
-        String kotaSmt[] = new String[5];
-
         for (int k = 0; k < arrKota.length; k++) {
 
             System.out.println("Kota ke " + k + " : " + arrKota[k]);
