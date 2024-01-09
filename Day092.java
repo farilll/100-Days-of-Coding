fac80547864cf73f6a9fd086e7fd34617bc2a456
farilll <146690181+farilll@users.noreply.github.com>
2024-01-09 18:41:48 +0700

    vector

---
+++ Day092.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+public class Day092 {
+    public static void main(String[] args) {
+        
+        
+        Vector <String> v = new Vector<String>();
+
+        v.add("a");
+        v.add("b");
+        v.add("c");
+        v.add("d");
+        v.add("e");
+        System.out.println("Vector : " + v);
+    }
+}
