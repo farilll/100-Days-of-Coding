31083a96f999681279b90dd34809e6cbb9e89803
farilll <146690181+farilll@users.noreply.github.com>
2024-01-09 18:52:25 +0700

    reserve

---
+++ Day093.java
@@ -0,0 +1,8 @@
+public class Day093 {
+    public static void main(String[] args) {
+        StringBuffer kata;
+        kata = new StringBuffer("udin very cool guy");
+        System.out.println("kata sebelum dibalik : " + kata);
+        System.out.println("kata sesudah dibalik : " + kata.reverse());
+    }
+}
