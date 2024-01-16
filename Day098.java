ce3d029b208e6e453eff80b95ea3401bf3c47960
farilll <146690181+farilll@users.noreply.github.com>
2024-01-16 16:20:11 +0700

    string equals

---
+++ Day098.java
@@ -0,0 +1,12 @@
+public class Day098 {
+    public static void main(String[] args) {
+        String a = new String("Jordan");
+        String b = new String("Jordan");
+
+        if (a.equals(b)){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
